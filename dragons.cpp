c23a8d0d1e9415a51f485ee503a23f557a0a544f
Max <44063879+tapchik@users.noreply.github.com>
2019-07-21 15:49:03 +0500

    Add files via upload
    Struggling with: 
    - Creating 2D vector
    - Managing 2D vectors (manipulating)
    - Creating sorted version of 2D vector "data"

---
+++ dragons.cpp
@@ -0,0 +1,27 @@
+//link to original problem: 
+//https://codeforces.com/problemset/problem/230/A
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+  
+  int s, n; 
+  cin >> s >> n;
+  
+  vector < vector<int> > data (n, vector<int> (2, 0));
+  
+  for (int i=0; i<n; i++)
+    cin >> data[i][0] >> data[i][1];
+  
+  vector < vector<int> > sorted;
+  
+  for (int i=0; i<n; i++) {
+    min 
+  }
+  
+  return 0;
+  
+}
